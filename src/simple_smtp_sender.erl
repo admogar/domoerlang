784ea5a61461f5df45f7016bba9c04dea93bfc43
omaralvarez <omar.alvarez@udc.es>
2013-12-21 13:53:27 +0100

    Email notifications when monitors go down SMTP sender with HTML message support

---
+++ src/simple_smtp_sender.erl
@@ -0,0 +1,37 @@
+-module(simple_smtp_sender).
+
+-export([send/4, send/5, send/6, send/7]).
+
+send(To, From, Subject, Message)->
+	Server="localhost",
+	send(To, From, Subject, Message, Server).
+
+send(To, From, Subject, Message, Server)->
+	PortNo=25,	
+	send(To, From, Subject, Message, Server, PortNo).
+
+send(To, From, Subject, Message, Server, PortNo)->
+	{ok, Hostname}=inet:gethostname(),
+	send(To, From, Subject, Message, Server, PortNo, Hostname).
+
+send(To, From, Subject, Message, Server, PortNo, LocalDomain)->
+	{ok,Sock}=gen_tcp:connect(Server,PortNo,[{active, false}]),
+	{ok, "220"++_}=gen_tcp:recv(Sock, 0),
+	gen_tcp:send(Sock, lists:append(["HELO ", LocalDomain, [13, 10]])),
+	{ok, "250"++_}=gen_tcp:recv(Sock, 0),
+	gen_tcp:send(Sock, lists:append(["MAIL FROM: ", From, [13, 10]])),
+	{ok, "250"++_}=gen_tcp:recv(Sock, 0),
+	gen_tcp:send(Sock, lists:append(["RCPT TO: ", To, [13, 10]])),
+	{ok, "250"++_}=gen_tcp:recv(Sock, 0),
+	gen_tcp:send(Sock, lists:append(["DATA", [13, 10]])),
+	{ok, "354"++_}=gen_tcp:recv(Sock, 0),
+	gen_tcp:send(Sock, lists:append(["Subject: ", Subject, [13, 10]])),
+	gen_tcp:send(Sock, lists:append(["Mime-Version: 1.0;",[13, 10]])),%%Para poner HTML en emails
+	gen_tcp:send(Sock, lists:append(["Content-Type: text/html;",[13, 10],[13, 10]])),
+	gen_tcp:send(Sock, lists:append([Message])),
+	gen_tcp:send(Sock, lists:append([[13, 10],".",[13, 10],[13, 10]])),
+	{ok, "250"++_}=gen_tcp:recv(Sock, 0),
+	%gen_tcp:send(Sock,lists:append("QUIT", [13, 10])), %%Comentado porque nuestro server SMTP no lo soporta
+	%{ok, "221"++_}=gen_tcp:recv(Sock, 0),
+	gen_tcp:close(Sock),
+	ok.	
