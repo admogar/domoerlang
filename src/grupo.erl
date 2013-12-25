73c5b066661b597e9704431e9851dfee67a546b3
Alfonso Nishikawa <alfonso.nishikawa@gmail.com>
2013-12-25 19:52:08 -0100

    Añadido el módulo de grupos y modificado el monitor para interactuar según el diagrama (Google Drive) y según la máquina de estados

---
+++ src/grupo.erl
@@ -0,0 +1,84 @@
+%% -*- coding: utf-8 -*-
+%%%-------------------------------------------------------------------
+%%% @author Omar Álvarez <omar.alvarez@udc.es>
+%%% @author Noelia Luaces <nluaces@gmail.com>
+%%% @author Adrián Morán <admogar@gmail.com>
+%%% @author Alfonso Nishikawa <alfonso.nishikawa@gmail.com>
+%%% @author David Torres <davidtorresandreu@gmail.com>
+%%% @doc
+%%%
+%%% =Nodo grupo=
+%%%
+%%% El master posee una serie de nodos grupo. Cada nodo grupo controla varios monitores.
+%%% 
+%%% El master ha de crear un nodo grupo e iterativamente asignar los monitores
+%%% a dicho grupo.     
+%%% 
+%%% == Funcionalidades ==
+%%% 
+%%% === Crear un grupo ===
+%%% El master *ha de crear el grupo*, siendo el master quien decide si desea enlazarse o no
+%%% (el enlazamiento dependerá de quién esté observando, no al revés).
+%%% 
+%%% Para ello se dispone de la función {@link crear_grupo/0}
+%%% Por cuestiones de comodidad se dispone también de la funcion {@link crear_y_enlazar/0} 
+%%%
+%%% === Añadir monitor al grupo ===
+%%% Para añadir un monitor se dispone de la función {@link añadir_monitor/1}
+%%%
+%%%
+%%%
+%%% @see client
+%%% @see monitor
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+-module(grupo).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([crear_grupo/0, crear_y_enlazar/0]).
+
+
+crear_grupo() ->
+    spawn(?MODULE, fun() -> init() end ,[]).
+
+crear_y_enlazar() ->
+    spawn_link(?MODULE, fun() -> init() end , []).
+    
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+% Ver documentación sobre conjuntos (módulo 'sets'): http://www.erlang.org/doc/man/sets.html
+
+
+init() ->
+    process_flag(trap_exit, true),
+    loop(sets:new()). % Lista vacía de monitores
+
+
+% Monitores :: set(PidMonitor,...)
+loop(Monitores) ->
+    receive
+
+        {_From, {anadir_monitor, PidMonitor}} ->
+            % Si ya contenemos el monitor, ignoramos
+            case sets:is_element(PidMonitor, Monitores) of
+                true -> loop(Monitores)
+                ; false ->
+                    % Si no contenemos el monitor lo añadimos y configuramos
+                    NuevosMonitores = sets:add_element(PidMonitor, Monitores),
+                    monitor:configurar_padre(PidMonitor, self()), % Monitor notificará a self
+
+                    loop(NuevosMonitores)
+            end
+        ; _ -> false
+    end.
+
+
+
+
+
--- src/monitor.erl
+++ src/monitor.erl
@@ -18,6 +18,8 @@
 %% PUBLIC API
 -export([start_link/2, init/2]).
 
+-export([configurar_padre/2 , start/1]).
+
 %%--------------------------------------------------------------------
 %% @doc Starts the monitor.
 %% @spec start_link(GroupPid :: pid(),
@@ -40,11 +42,41 @@ start_link(GroupPid, Type) -> % Type : bin | {num, Min, Max}
 %% @end
 %%--------------------------------------------------------------------
 init(GroupPid, State) ->
-    loop(GroupPid, State).
+    loop_stopped(GroupPid, State).
+
+
+%% Configura un Monitor para que envíe las notificaciones a un Grupo
+configurar_padre(PidMonitor, PidGrupo) ->
+    PidMonitor ! {self(), {configurar_padre, PidGrupo}}.
+
+%% Inicia el funcionamiento del monitor (transición del estado a 'start')
+start(PidMonitor) ->
+    PidMonitor ! {self(), {start}}.
+    
 
 %%% Internal implementation %%%
 
-loop(GroupPid, State) ->
+%%% ====================== Estado STOPPED ===============================
+loop_stopped(GroupPid,State) ->
+    receive
+    {From, ping} ->
+        From ! {self(), pong} ;
+        
+    {From, upgrade} ->
+        From ! {self(), ok},
+        ?MODULE:init(GroupPid, State);
+        
+    {_From, {configurar_padre, NuevoPidGrupo}} ->
+        loop_stopped(NuevoPidGrupo, State) ;
+
+    {_From, {start}} ->
+        loop_started(GroupPid, State)
+    
+    end.
+
+%%% ====================== Estado STARTED ===============================
+
+loop_started(GroupPid, State) ->
 % {GroupPid, Value}
 % {GroupPid, true}
 
@@ -52,36 +84,40 @@ loop(GroupPid, State) ->
 % {GroupPid, 5, {0, 10}}
     receive
 	{From, ping} ->
-	    From ! {self(), pong};
+	    From ! {self(), pong} ;
+        
 	{From, upgrade} ->
 	    From ! {self(), ok},
 	    ?MODULE:init(GroupPid, State);
+        
 	{From, getValue} ->
 	    case State of
-		{Value, Min, Max} -> % num
+		{Value, _Min, _Max} -> % num
 		    From ! {self(), Value},
-		    loop(GroupPid, State);
+		    loop_started(GroupPid, State);
 		Value -> % bin
 		    From ! {self(), Value},
-		    loop(GroupPid, State);
+		    loop_started(GroupPid, State);
 		false ->
 		    From ! badArgument, % FIXME
-		    loop(GroupPid, State)
+		    loop_started(GroupPid, State)
 	    end;
+        
 	{From, {setValue, NewValue}} ->
 	    case State of
 		{_Value, Min, Max} ->
-		    loop(GroupPid, {NewValue, Min, Max});
+		    loop_started(GroupPid, {NewValue, Min, Max});
 		_Value ->
 		    GroupPid ! {self(), NewValue},
-		    loop(GroupPid, NewValue);
+		    loop_started(GroupPid, NewValue);
 		false ->
 		    From ! error, % FIXME
-		    loop(GroupPid, State)
+		    loop_started(GroupPid, State)
 	    end
+
     after ?TIMEOUT ->
 	    GroupPid ! {self(), heartbeat},
-	    loop(GroupPid, State)
+	    loop_started(GroupPid, State)
     end.
 
 % TODO Percentual change function
